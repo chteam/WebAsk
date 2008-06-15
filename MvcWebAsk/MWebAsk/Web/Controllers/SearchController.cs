d9ea3690f835df95f978819bb34e257767d9bdfe
chsword_cp <SND\chsword_cp>
2008-06-15 16:43:41 +0000

    功能第一阶段版本

---
+++ MvcWebAsk/MWebAsk/Web/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MWebAsk.Controllers {
+	public class SearchController : DBController {
+		public ActionResult Index(string keyword,int mod) {
+
+			List<MWebAsk.Models.Question> ret;
+			if(mod==1)
+			ret= (from q in DB.Question
+					   where q.Title.Contains(keyword)
+					   select q).ToList();
+			else
+				ret = (from q in DB.Reply
+					   join r in DB.Question on q.QuestionID equals r.ID
+					   where q.Body.Contains(keyword)
+					   select r).ToList();
+		
+			// Add action logic here
+			return View(ret);
+		}
+	}
+}
---
+++ MvcWebAsk/MWebAsk/Web/Views/Search/Index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/ViewMaster.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="MWebAsk.Views.Search.Index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentBody" runat="server">
+
+	<%
+		foreach (Question q in ViewData.Model) {
+		 %>
+		 
+	<li>
+	<%=Html.ActionLink<QuestionController>(c=>c.Index(q.ID),q.Title) %>
+	<%=q.Addtime.ToString("Mm dd/hh:mm") %> <%=q.UserID %>
+	<%=Enum.GetName(QType.待解决.GetType(),q.Situation) %>
+	</li>
+	<%} %>
+</asp:Content>
