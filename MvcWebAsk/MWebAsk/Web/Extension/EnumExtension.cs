33ca5aeaecaa2335b1c9d3dd322becb33cee2749
chsword_cp <SND\chsword_cp>
2008-06-15 14:47:27 +0000

    2 个扩展方法的错

--- MvcWebAsk/MWebAsk/Web/Extension/EnumExtension.cs
+++ MvcWebAsk/MWebAsk/Web/Extension/EnumExtension.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 namespace MWebAsk {
+	/// <summary>
+	/// 问题 和回复的扩展方法,将Enum转为byte
+	/// </summary>
 	static public class EnumExtension {
 		static public byte ToByte(this QType e) {
 			return (byte)e;
