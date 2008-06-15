3e073d42ba96bbf29dc05f2c388c59f2ff7dd77e
chsword_cp <SND\chsword_cp>
2008-06-15 12:53:17 +0000

    问题回复状态注释

--- MvcWebAsk/MWebAsk/Web/Models/Enum/QType.cs
+++ MvcWebAsk/MWebAsk/Web/Models/Enum/QType.cs
@@ -1,9 +1,15 @@
 ﻿namespace MWebAsk {
+	/// <summary>
+	/// 问题状态Question.Situation
+	/// </summary>
 	public enum QType {
 		待解决 = 0,
 		过期 = 200,
 		已解决 = 255
 	}
+	/// <summary>
+	/// 回复状态 Reply.Situation
+	/// </summary>
 	public enum AType {
 		回复 = 0,
 		最佳答案 = 255
